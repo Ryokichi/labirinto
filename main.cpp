93f53504e4bed03dfc42b4a04af0f9656932051c
Heitor Ryokichi Nakamura <heitor.nakamura@hotmail.com>
2018-05-15 00:02:29 -0300

    Formação do labirinto por imagem, ao inves de escrever array na mão

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 #define ESC    	27
 
-
-////Variaveis globais
-int l1[10][20], l2[10][20], l3[10][20];
-
 struct Player{
     int x = 0;
 	int y = 0;
@@ -18,53 +14,37 @@ struct Img{
 	void *a = malloc(imagesize(0,0,63,63));	
 };
 
+////Variaveis globais
+int l1[10][20], l2[10][20], l3[10][20];
 Img *img_pool = NULL;
-//char *img = NULL;
+
 
 ////Funções
 void carregaLabirinto();
 void desenhaCena(int cena, int lab[10][20]);
-void carregaImagens();
+void carreImagens();
 ////
 
 int main(){
 	printf("Integrantes: \nCAROLINE GONCALVES DE FELIPE \nHEITOR RYOKICHI NAKAMURA \nRENAN ALVES NOWAK \nWILSON RICARDO DA SILVA FABOZI\n\n");
 
 	Player jogador;
-	int pg, tecla, cena_atual;	
-	int img_size;
-	char path[4096], c_aux[4096];
+	int pg, tecla, cena_atual;
 	
 	initwindow(1300, 660, "Labirinto", 0, 0);
-	setbkcolor(RGB(25,145,25));	
+	setbkcolor(RGB(25,145,25));		
 	
-	img_size = imagesize(0,0,63,63);
-		
-	for(int i=1; i<=2; i++){				 
-		img_pool = (Img*)realloc(img_pool, sizeof(Img)*i);
-		Img img;
-				
-		strcpy(path, "img/tile_");
-		sprintf(c_aux, "%d", i);
-		strcat(path, c_aux);
-		strcat(path, ".bmp");
-		
-		setactivepage(2);
-		readimagefile(path, 0, 0, 63, 63);
-		getimage(0, 0, 63, 63, img.a);
-		
-		img_pool[i-1] = img;		
-	}
+	setactivepage(2);
+	carregaLabirinto();
+	carreImagens();	
 	
-	cena_atual = 1;		
-	
+	cena_atual = 1;
 	while (tecla != ESC){
 		if (pg==0) pg=1; else pg=0;
         setactivepage(pg);
-        cleardevice();
-        carregaLabirinto();	
+        cleardevice();       	
                 
-        //desenhaCena(cena_atual, l1);
+        desenhaCena(cena_atual, l1);
         
         setvisualpage(pg);
         if (kbhit()) tecla = getch();
@@ -84,17 +64,44 @@ void desenhaCena(int cena, int lab[10][20]){
 }
 
 void carreImagens(){
+	char path[4096], c_aux[4096];
+	int img_size = imagesize(0,0,63,63);
 	
+	for(int i=1; i<=2; i++){				 
+		img_pool = (Img*)realloc(img_pool, sizeof(Img)*i);
+		Img img;
+				
+		strcpy(path, "img/tile_");
+		sprintf(c_aux, "%d", i);
+		strcat(path, c_aux);
+		strcat(path, ".bmp");
+		
+		setactivepage(2);
+		readimagefile(path, 0, 0, 63, 63);
+		getimage(0, 0, 63, 63, img.a);
+		
+		img_pool[i-1] = img;		
+	}	
 }
+
 void carregaLabirinto(){
-	void *lab;	
-	setactivepage(2);
+	void *lab;
+	lab = malloc(imagesize(0,0,20,10));
+	int cor;
+	
 	readimagefile("img/lab_1.bmp", 0, 0, 20, 10);
 	getimage(0, 0, 20, 10, lab);
-	
-	//for (int i=0; i<10; i++){
-		//for (int j=0; j<20; j++){
-			//printf(" cor = %d \n", getpixel(i,j));
-		//}
-	//}
+		
+	for (int i=0; i<10; i++){
+		for (int j=0; j<20; j++){
+			cor = getpixel(j,i);
+			if (cor==0){
+				l1[i][j] = 1;
+			}
+			else {
+				l1[i][j] = 0;
+			}			
+			printf("i=%d, j=%d | cor = %d | %d\n", i, j, &cor, RGB(0,255,255));
+		}
+	}
 }
